f1e798f0c0f51582bd5c4083694638980cfceff8
SaiFullStackDeveloper <satya39sai@gmail.com>
2025-08-11 13:24:49 +0530

    first commit

---
+++ src/app/demo/dashboard/default/default.component.ts
@@ -0,0 +1,82 @@
+// Angular Import
+import { Component } from '@angular/core';
+
+// project import
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { BajajChartComponent } from 'src/app/theme/shared/components/apexchart/bajaj-chart/bajaj-chart.component';
+import { BarChartComponent } from 'src/app/theme/shared/components/apexchart/bar-chart/bar-chart.component';
+import { ChartDataMonthComponent } from 'src/app/theme/shared/components/apexchart/chart-data-month/chart-data-month.component';
+
+@Component({
+  selector: 'app-default',
+  imports: [BajajChartComponent, BarChartComponent, ChartDataMonthComponent, SharedModule],
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.scss']
+})
+export class DefaultComponent {
+  // public method
+ 
+
+  ListGroup = [
+    {
+      name: 'Car Insurance',
+      profit: '5% Loss',
+      invest: '10-08-2025',
+      bgColor: 'bg-light-danger',
+      icon: 'ti ti-chevron-down',
+      color: 'text-danger'
+    },
+    {
+      name: 'Health Insurance',
+      profit: '8% Loss',
+      invest: '05-08-2025',
+      bgColor: 'bg-light-danger',
+      icon: 'ti ti-chevron-down',
+      color: 'text-danger'
+    },
+    {
+      name: 'Home Insurance',
+      profit: '2% Profit',
+      invest: '01-08-2025',
+      bgColor: 'bg-light-success',
+      icon: 'ti ti-chevron-up',
+      color: 'text-success'
+    },
+    {
+      name: 'Travel Insurance',
+      profit: '12% Loss',
+      invest: '28-07-2025',
+      bgColor: 'bg-light-danger',
+      icon: 'ti ti-chevron-down',
+      color: 'text-danger'
+    },
+    {
+      name: 'Pet Insurance',
+      profit: '3% Profit',
+      invest: '20-07-2025',
+      bgColor: 'bg-light-success',
+      icon: 'ti ti-chevron-up',
+      color: 'text-success',
+      space: 'pb-0'
+    }
+  ];
+  
+
+  profileCard = [
+    {
+      style: 'bg-primary-dark text-white',
+      background: 'bg-primary',
+      value: '$203k',
+      text: 'Net Profit',
+      color: 'text-white',
+      value_color: 'text-white'
+    },
+    {
+      background: 'bg-warning',
+      avatar_background: 'bg-light-warning',
+      value: '$550K',
+      text: 'Total Revenue',
+      color: 'text-warning'
+    }
+  ];
+}
